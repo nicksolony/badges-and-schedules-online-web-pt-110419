2369d52e7911f90f7a4f29e3d9f5a081dd0835ce
Mykola Solonyy <nicksolony@gmail.com>
2019-11-07 03:04:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,10 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (array)
+def batch_badge_creator (names)
+  badges= []
+  names.each {|name| badges << "Hello, my name is #{name}."}
+  badges
   
 end
 
